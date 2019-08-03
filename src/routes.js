829c46f587ce93d1c1cf8a9c0dc939b79f7b0fdf
tupizz <tadeu.tupiz@gmail.com>
2019-08-03 17:19:25 -0300

    :package: Sequelize configs

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) =>
-    res.json({
-        hello: 'world',
-    })
-);
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Tadeu Tupinambá',
+        email: 'tadu.tupiz@gmail.com',
+        password_hash: '123467890',
+    });
+
+    res.json(user);
+});
 
 export default routes;
